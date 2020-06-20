3ff3a4239ffbd98da80f7ce7b2b6a90b0ada4619
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-06-20 14:00:45 -0400

    added .js to routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/index");
+const Workout = require("../models/Workout.js");
 
 router.post("/api/workouts", (req, res) => {
     Workout.create({})
