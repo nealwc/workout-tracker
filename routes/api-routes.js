d4b15a5919ca72a8db01dfd6127b84b5ae62c784
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-06-20 09:56:34 -0400

    changed workout.js to workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/Workout.js");
+const Workout = require("../models/Workout");
 
 router.post("/api/workouts", (req, res) => {
     Workout.create({})
